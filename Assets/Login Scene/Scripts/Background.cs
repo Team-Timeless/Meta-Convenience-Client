94ba3643bf6ff1bd51f69f65568fd68dcd4e5c5f
sunforest99 <taeyang9300@daum.net>
2022-04-23 23:03:55 +0900

    ADD : 키보드 쉬프트, 입력 구현

--- Assets/Login Scene/Scripts/Background.cs
+++ Assets/Login Scene/Scripts/Background.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class Background : MonoBehaviour
@@ -18,10 +19,42 @@ public class Background : MonoBehaviour
     [SerializeField]
     private float minAlpha = 0.25f;     // 알파값 최소값
 
+    // ---- 키보드 UI (나중에 vr 예외 처리 하기)
+    [SerializeField]
+    private StringBuilder sb = new StringBuilder();
+
+    static private bool isShift = false;
+
+    [SerializeField]
+    private GameObject keybord = null;
+
+    [SerializeField]
+    private GameObject[] special = new GameObject[2];
+
+    [SerializeField]
+    private List<UnityEngine.UI.InputField> input = new List<UnityEngine.UI.InputField>();
+
+    public static bool Shift
+    {
+        get
+        {
+            return isShift;
+        }
+    }
+
     void Start()
     {
         spriterenderer.sprite = backGround[0];
         StartCoroutine("FadeIn");
+    }
+
+    void Update()
+    {
+        setIinputField();
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            NetworkMng.I.Login();
+        }
     }
 
     /**
@@ -36,7 +69,7 @@ public class Background : MonoBehaviour
         }
 
         index++;
-        if (index.Equals(backGround.Length)) { index = 0;}
+        if (index.Equals(backGround.Length)) { index = 0; }
         spriterenderer.sprite = backGround[index];
         StartCoroutine("FadeOut");
     }
@@ -52,6 +85,92 @@ public class Background : MonoBehaviour
             yield return new WaitForSeconds(fadeSpeed);
         }
         StartCoroutine("FadeIn");
+    }
 
+    // ui 부분 (키보드)
+
+    /**
+     * brief 키보드 버튼 클릭 했을떄
+     * @param 키보드 버튼 obj 가져와서 이름을 문자열로 만들어주기
+     */
+    public void keyclick(UnityEngine.UI.Button button)
+    {
+        sb.Append(button.name);
+        input[0].text = sb.ToString();
+    }
+
+    /**
+     * @brief 쉬프트 클릭 하면 특수문자, 숫자 전환
+     */
+    public void shiftClick()
+    {
+        if (isShift)
+        {
+            special[0].SetActive(false);        // 특수문자
+            special[1].SetActive(true);         // 숫자
+            isShift = false;
+        }
+        else
+        {
+            special[0].SetActive(true);
+            special[1].SetActive(false);
+            isShift = true;
+        }
+    }
+
+    /**
+     * @biref 백 스페이스 구현
+     */
+    public void backSpace()
+    {
+        if(sb.Length > 0) { sb.Length--; }
+        input[0].text = sb.ToString();
+    }
+
+    /**
+     * @brief Enter 버튼 누르면 로그인
+     */
+    public void returnBtn()
+    {
+        NetworkMng.I.Login();
+    }
+
+    /**
+     * @brief 스페이스 (띄어쓰기) 구현
+     */
+    public void Space()
+    {
+        sb.Append(" ");
+        input[0].text = sb.ToString();
+    }
+
+    /**
+     * @brief 키보드 뒤로가기 버튼 구현
+     */
+    public void backBtn()
+    {
+        keybord.SetActive(false);
+        input.Clear();
+    }
+
+    /**
+     * @brief 문자열 초기화 후 클릭한 인풋 필드 저장
+     */
+    void setIinputField()
+    {
+        if (NetworkMng.I.inputfildId.isFocused)
+        {
+            sb.Clear();
+            keybord.SetActive(true);
+            if (input.Count != 0) { input.RemoveAt(0); }
+            input.Add(NetworkMng.I.inputfildId);
+        }
+        else if (NetworkMng.I.inputfildPwd.isFocused)
+        {
+            sb.Clear();
+            keybord.SetActive(true);
+            if (input.Count != 0) { input.RemoveAt(0); }
+            input.Add(NetworkMng.I.inputfildPwd);
+        }
     }
 }
